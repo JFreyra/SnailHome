50fcef2d9e687dd2713a7c755555ef0b6fe8ef42
JFreyra <efreyra@stuy.edu>
2016-11-21 15:33:56 -0500

    added mainShell file

--- snailShell.c
+++ snailShell.c
@@ -5,30 +5,39 @@
 
 int main() {
 
-  char *input[256];
-  printf(">>");
-  fgets(input,sizeof(input),stdin);
+  int exit = 0;
   
-  if (!strcmp(input,"\n")) {
-    printf("Please enter a valid argument!\n");
-    return 0;
+  while(!exit){
+    char *input[256];
+    printf(">>");
+    fgets(input,sizeof(input),stdin);
+    
+    if (!strcmp(input,"\n")) {
+      printf("Please enter a valid argument!\n");
+      return 0;
+    }
+    
+    strtok(input,"\n");
+  
+
+    char *s = input;
+    char *commands[10];
+  
+    int i = 0;
+    while (s) {
+      commands[i] = strsep(&s," ");
+      i++;
+    }
+
+    if(!strcmp(command[0],"exit")){
+      exit = 1;
+      break;
+    }
+
+
+    execvp(commands[0],commands);
   }
-  
-  strtok(input,"\n");
 
-
-  char *s = input;
-  char *commands[10];
-
-  int i = 0;
-  while (s) {
-    commands[i] = strsep(&s," ");
-    i++;
-  }
-
-
-  execvp(commands[0],commands);
-  
   return 0;
 
   
