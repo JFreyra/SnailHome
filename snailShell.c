51bb860867f7dda691547f1e8bb0316ee472b15f
LingCheng3273 <lcheng3@stuy.edu>
2016-11-23 15:10:58 -0500

    Added cd method
    Doesn't work for number directories

--- snailShell.c
+++ snailShell.c
@@ -29,13 +29,97 @@ int main() {
       i++;
     }
 
-    if(!strcmp(command[0],"exit")){
+    if(!strcmp(commands[0],"exit")){
       exit = 1;
       break;
     }
 
+     if(!strcmp(commands[0],"cd")){
+      //gets your current path
+      char path[10];
+      getcwd(path, 100);
+      printf("This is your current path: %s\n", path);
+        
+      char *tempPath= path;
+      char *dirs[100];//contains directories to the path
+      i = 0;
+      strsep(&tempPath,"/");//gets rid of first "" element
+        
+      //parses through the path
+      while (tempPath) {
+	dirs[i] = strsep(&tempPath,"/");
+	i++;
+      }
+      dirs[i]=0;
+        
+      //prints out the directories to path
+      /*int i=0;
+      while(dirs[i]){
+	printf("directory: %s\n", dirs[i]);
+	i++;
+      }*/
+      
+      /*i=0;
+      while(dirs[i]){
+	printf("directory: %s\n", dirs[i]);
+	i++;
+	}*/
 
-    execvp(commands[0],commands);
+        
+      //creates new path according to input
+      char newpath[100];
+      strcpy(newpath, "/");
+      
+      /*i=0;
+      while(dirs[i]){
+	printf("directory: %s\n", dirs[i]);
+	i++;
+      }*/
+      
+      //printf("directory: %s\n", dirs[2]);
+      
+      i=0;
+
+      if (!strcmp(commands[1],"..")){
+	printf("command is ..\n");
+	while (dirs[i]) {
+	  printf("adding: %s\n",dirs[i]);
+	  strcat(newpath, dirs[i]);
+	  strcat(newpath, "/");
+	  printf("the path: %s\n",newpath );
+	  i++;
+	}    
+      }else{
+	printf("command not ..\n");
+	while (dirs[i]) {
+	  printf("adding: %s\n",dirs[i]);
+	  strcat(newpath, dirs[i]);
+	  strcat(newpath, "/");
+	  printf("the path: %s\n",newpath );
+	  i++;
+	}
+      }
+        
+      strcat(newpath, commands[1]);
+      printf("the new path: %s\n", newpath);
+        
+      //changes directory
+      printf("Error?: %d\n", chdir(newpath));
+      getcwd(path, 100);
+      printf("This is your current path now: %s\n", path);
+       
+      /*printf("Error?: %d\n", chdir("/home/cg"));
+        getcwd(path, 100);
+        printf("This is your current path (cg): %s\n", path);
+        printf("Error?: %d\n", chdir("/home/cg/root"));
+        getcwd(path, 100);
+        printf("This is your current path (root): %s\n", path);
+      */
+    }
+    
+
+
+    //execvp(commands[0],commands);
   }
 
   return 0;
