02b24fb1098f6ac8a0ca0dd4a06e53ef9ef68918
JFreyra <efreyra@stuy.edu>
2016-11-25 21:33:34 -0500

    organized, merged, we need to do the piping/redirection stuff

---
+++ makefile
@@ -0,0 +1,9 @@
+all: snailShell.c
+	gcc -o snailShell snailShell.c
+
+run: all
+	./snailShell
+
+clean:
+	rm snailShell
+	rm *~
--- snailShell.c
+++ snailShell.c
@@ -2,22 +2,84 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+// gets your current path
+void cd(char *commands[]){
+  
+  char path[100];
+  getcwd(path, 100);
+  printf("This is your current path: %s\n", path);
+        
+  char *tempPath= path;
+  char *dirs[100];//contains directories to the path
+  int i = 0;
+  strsep(&tempPath,"/");//gets rid of first "" element
+        
+  //parses through the path
+  while (tempPath) {
+    dirs[i] = strsep(&tempPath,"/");
+    i++;
+  }
+  dirs[i]=0;
+      
+  //creates new path according to input
+  char newpath[100];
+  strcpy(newpath, "/");
+  i=0;   
+  if (!strcmp(commands[1],"..")){
+    while (dirs[i]) {
+      //printf("adding: %s\n",dirs[i]);
+      strcat(newpath, dirs[i]);
+      strcat(newpath, "/");
+      //printf("the path: %s\n",newpath );
+      i++;
+    }    
+  }else{
+    while (dirs[i]) {
+      // printf("adding: %s\n",dirs[i]);
+      strcat(newpath, dirs[i]);
+      strcat(newpath, "/");
+      // printf("the path: %s\n",newpath );
+      i++;
+    }
+  }  
+  strcat(newpath, commands[1]);
+      
+  //changes directory
+  if(chdir(newpath))
+    printf("Error?: %d Cannot Change to this Directory\n", chdir(newpath));
+  getcwd(path, 100);
+  printf("This is your new path: %s\n", path);
+  
+}
+
+void PorR(){
+
+}
 
 int main() {
 
-  int exit = 0;
-  
-  while(!exit){
-    char *input[256];
-    printf(">>");
+  while(1){
+    char input[256];
+    char path[666];
+    getcwd(path, 666);
+    printf("%s >> ", path);
     fgets(input,sizeof(input),stdin);
     
     if (!strcmp(input,"\n")) {
-      printf("Please enter a valid argument!\n");
-      return 0;
+      continue;
     }
-    
-    *(strchr(input, '\n')) = NULL; //strtok(input,"\n");
+
+    int PR = 0;
+    if(strchr(input,'>') || strchr(input,'<') || strchr(input,'|'))
+      PR = 1;
+
+    /*
+    char * newLine=strchr(input, '\n');
+    if (newLine)
+      *newLine = NULL; */
+    strtok(input,"\n");
   
 
     char *s = input;
@@ -26,101 +88,35 @@ int main() {
     int i = 0;
     while (s) {
       commands[i] = strsep(&s," ");
+      //printf("%s \n", commands[i]);
       i++;
     }
+    commands[i]=NULL;
 
     if(!strcmp(commands[0],"exit")){
-      exit = 1;
-      break;
+      exit(0);
     }
 
-     if(!strcmp(commands[0],"cd")){
-      //gets your current path
-      char path[100];
-      getcwd(path, 100);
-      printf("This is your current path: %s\n", path);
-        
-      char *tempPath= path;
-      char *dirs[100];//contains directories to the path
-      i = 0;
-      strsep(&tempPath,"/");//gets rid of first "" element
-        
-      //parses through the path
-      while (tempPath) {
-	dirs[i] = strsep(&tempPath,"/");
-	i++;
-      }
-      dirs[i]=0;
-        
-      //prints out the directories to path
-      int i=0;
-      /*while(dirs[i]){
-	printf("directory: %s\n", dirs[i]);
-	i++;
-	}*/
-      
-      /*i=0;
-      while(dirs[i]){
-	printf("directory: %s\n", dirs[i]);
-	i++;
-	}
-      */
-        
-      //creates new path according to input
-      char newpath[100];
-      strcpy(newpath, "/");
-      
-      /*i=0;
-      while(dirs[i]){
-	printf("directory: %s\n", dirs[i]);
-	i++;
-	}*/
-      
-      //printf("directory: %s\n", dirs[2]);
-      
-      i=0;
-
-      if (!strcmp(commands[1],"..")){
-	while (dirs[i]) {
-	  //printf("adding: %s\n",dirs[i]);
-	  strcat(newpath, dirs[i]);
-	  strcat(newpath, "/");
-	  //printf("the path: %s\n",newpath );
-	  i++;
-	}    
-      }else{
-	while (dirs[i]) {
-	  // printf("adding: %s\n",dirs[i]);
-	  strcat(newpath, dirs[i]);
-	  strcat(newpath, "/");
-	  // printf("the path: %s\n",newpath );
-	  i++;
-	}
-      }
-        
-      strcat(newpath, commands[1]);
-      //printf("the new path: %s\n", newpath);
-        
-      //changes directory
-      printf("Error?: %d\n", chdir(newpath));
-      getcwd(path, 100);
-      printf("This is your new path: %s\n", path);
-       
-      /*printf("Error?: %d\n", chdir("/home/cg"));
-        getcwd(path, 100);
-        printf("This is your current path (cg): %s\n", path);
-        printf("Error?: %d\n", chdir("/home/cg/root"));
-        getcwd(path, 100);
-        printf("This is your current path (root): %s\n", path);
-      */
+    if(!strcmp(commands[0],"cd")){
+      cd(commands);
+      continue;
     }
-    
+      
+    int pid=fork();
 
+    if (!pid){
+      if(PR)
+	PorR();
+      else{
+	execvp(commands[0],commands);
+	printf("%s \n", strerror(errno));
+      }
 
-    //execvp(commands[0],commands);
+    }
+    if (pid)
+      waitpid(pid);
+
   }
-
-  return 0;
-
+  
   
 }
