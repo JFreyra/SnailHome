b4910f154c49584ebf7c440bdd708dd7933fca12
plwslw <s.lit.winkler@gmail.com>
2016-11-28 00:47:31 -0500

    Redirection works in theory. In reality, not so much.

--- snailShell.c
+++ snailShell.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
 // Takes a path directly as it is received from the input.
 // changes directory to that path.
@@ -56,37 +58,62 @@ void cd(char *commands[]){
   
 }
 
-//Takes in the
-void command(char* path, char* args [], fd in, fd out){
+//Takes in the arguments to execvp as well as file descriptors for the input and output. 
+void command(char* path, char* args [], int in, int out){
+  //printf("still alive?\n");
+
+  /*
+  //  int std_in =dup(STDIN_FILENO);
+  //int std_out =dup(STDOUT_FILENO);
+  if (in != STDIN_FILENO){
+    dup2(in, STDIN_FILENO);
+  }  
+    
+  if (out != STDOUT_FILENO){
+    dup2(out, STDOUT_FILENO);
+  }
+  */
+  
+  //printf("how bout now?\n");
+
+  int pid=fork();
+
+  if (!pid){
     if (in != STDIN_FILENO){
-        dup2(in, STDIN_FILENO);
+      dup2(in, STDIN_FILENO);
+      close(in);
     }  
     
     if (out != STDOUT_FILENO){
-        dup2(out, STDOUT_FILENO);
-    } 
-    
-    int pid=fork();
-
-    if (!pid){
-	  execvp(path, args);
-	  printf("%s \n", strerror(errno));
+      dup2(out, STDOUT_FILENO);
+      close(out);
     }
     
-    if (pid)
-      waitpid(pid);
+    execvp(path, args);
+    printf("%s \n", strerror(errno));
+    exit(1);
+  }
+    
+  if (pid){
+    int s;
+    wait(&s);
+  }
 
 }
 
 void main() {
+  umask(0000);
 
   while(1){
-    char input[256];
-    char path[666];
+    //char input[256];
+    //char path[666];
+    
+    char *path=malloc(sizeof(char*));
+    char *input=malloc(sizeof(char*));
     getcwd(path, 666);
     printf("%s >> ", path);
-    fgets(input,sizeof(input),stdin);
-    
+    fgets(input,256,stdin);
+    //printf("%s\n", input);
     if (!strcmp(input,"\n")) {
       continue;
     }
@@ -103,30 +130,41 @@ void main() {
       *newLine = NULL; */
     strtok(input,"\n");
   
-//Searches for < or >
-    fd source, dest;
+//Searches for <
+    int source, dest;
     
     if (strchr(input, '<')){
-        char * path=strsep(*input, '<');
+        char * path=strsep(&input, "'<'");
         //Clear leading whitespace of files
-        while(strcmp(path[0]," ") != 0)
-            path++;
-        //File came first --> swap source and file.
-        char * temp = path;
-        path = input;
-        input = temp;
-        
-        source = open(source, O_CREAT, O_WRONLY);
+        while(path[0] != ' ')
+	  path++;
+
+	source=open(path, O_RDONLY);
     }
     else source = STDIN_FILENO;
     
     if (strchr(input, '>')){
-        char * path2 = strsep(*input, "'>'");
-        while(strcmp(path2[0]," ") != 0)
+      //printf("%s\n",input);
+      char * path2 = strsep(&input, "'>'");
+
+      printf("ouput file: %s\n", path2);
+      printf("???: %s\n", input);
+      //File came first --> swap source and file.
+
+      char *temp=path2;
+      path2=input;
+      input=temp;
+      
+      printf("ouput file: %s\n", path2);
+      printf("???: %s\n", input);
+
+      while(path2[0] == ' ')
             path2++;
             
-        dest = open(dest, O_CREAT, O_WRONLY);
-        
+      dest = open(path2, O_CREAT, 0644);
+      close(dest);
+      open(path2, O_WRONLY);
+      //printf("sadface \n");
     }
     else dest = STDOUT_FILENO;
     
@@ -142,7 +180,7 @@ void main() {
     commands[i]=NULL;
 
     if(!strcmp(commands[0],"exit")){
-      exit(0);
+      exit(1);
     }
 
     if(!strcmp(commands[0],"cd")){
